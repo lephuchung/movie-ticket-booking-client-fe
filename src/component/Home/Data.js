475f1a82db56ebe21318731b3a6cef5dcc9ac6e6
NongPhiHung20194581 <hung.np194581@sis.hust.edi.vn>
2024-11-04 18:42:39 +0700

    movie carousel

---
+++ src/component/Home/Data.js
@@ -0,0 +1,11 @@
+export const movie = [
+    {
+        image: require('./Image/1.jpg'), 
+    },
+    {
+        image: require('./Image/2.png'),
+    },
+    {
+        image: require('./Image/3.jpg'),
+    },
+];
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Home.scss';
+import Carousel from './Carousel';
+import { movie } from './Data';
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div>Home</div>
+      <Carousel images={movie}/>
+    </div>
   )
 }
 
