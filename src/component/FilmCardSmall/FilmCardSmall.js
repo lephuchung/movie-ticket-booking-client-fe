708d527e54c0f27717ac720a95dd4e1126df5bc2
lephuchung <lephuchung003@gmail.com>
2024-11-15 00:22:37 +0700

    Update css film detail

---
+++ src/component/FilmCardSmall/FilmCardSmall.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// import poster from "./image.png"
+import "./FilmCardSmall.scss"
+import { Link } from 'react-router-dom'
+
+const FilmCardSmall = ({ film }) => {
+
+    // const film = {
+    //     title: "Venom: Kèo Cuối",
+    //     rating: "8.5/10",
+    //     poster: "https://cdn.galaxycine.vn/media/2024/10/30/the-paradise-of-thorns-1_1730262436681.jpg"
+    // }
+
+    return (
+        <Link to={`/film/${film.title}`} className='film-card-small-link'>
+            <div className='film-card-small'>
+                <img src={film?.poster} alt="" className='film-card-small-poster' />
+                <div className='film-card-small-information'>
+                    <span className='film-card-small-title'>{film.title}</span>
+                    <span className='film-card-small-rating'>{film.rating}/10</span>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default FilmCardSmall
