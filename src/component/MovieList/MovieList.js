341ac9553016235b141e17ab1744a06387113298
lephuchung <lephuchung003@gmail.com>
2024-11-16 05:02:52 +0700

    Update film page

--- src/component/MovieList/MovieList.js
+++ src/component/MovieList/MovieList.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './MovieList.scss';
 import FilmCard from '../FilmCard/FilmCard';
 
-const MovieList = ({ genre, movies }) => {
+const MovieList = ({ genre, movies, all = false }) => {
   return (
     <div className="movie-list">
       {genre &&
         <div className='movie-list-title-area'>
           <h2 className='movie-list-title'>{genre}</h2>
-          <button className='movie-list-title-button'>Tất cả</button>
+          {!all &&
+            <button className='movie-list-title-button'>Tất cả</button>
+          }
         </div>
       }
       <div className='movie-list-content'>
--- src/page/Film/Film.js
+++ src/page/Film/Film.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./Film.scss"
+import MovieList from '../../component/MovieList/MovieList'
+import { movies, moviesHumour, moviesAction } from '../Data'
 
 const Film = () => {
+  const all = true;
   return (
-    <div>Film</div>
+    <div className='film-page-container'>
+      <MovieList genre={"Danh sách phim"} movies={movies} all={all} />
+    </div>
   )
 }
 
