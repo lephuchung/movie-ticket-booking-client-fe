39fc42db12537218aeccd19440b9e8d0319bb21e
lephuchung <lephuchung003@gmail.com>
2024-12-27 00:21:46 +0700

    Fixed qr code

--- src/page/Payment/Payment.js
+++ src/page/Payment/Payment.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "./Payment.scss"
 import { QRCodeCanvas } from 'qrcode.react';
+import QRcode from "./QRCode.jpg"
 import { Base64 } from 'js-base64';
 import { useNavigate, useParams } from 'react-router';
 import { fetchShowtimeDetail } from '../../apis/fetchShowtimeDetail';
@@ -10,12 +11,7 @@ import { fetchFilmById } from '../../apis/fetchFilmById';
 import { getDateHourMinuteFromISOTime } from '../../utils/getDayHourMinuteFromIsoTime';
 
 const Payment = () => {
-    const orderInfo = {
-        orderId: "123456",
-        customerName: "Nguyễn Văn A",
-        totalAmount: "500,000 VNĐ",
-        orderDate: "26/12/2024",
-    };
+
     const navigate = useNavigate();
     const { encodedData } = useParams();
     const [decodedData, setDecodedData] = useState(null);
@@ -27,7 +23,6 @@ const Payment = () => {
     const userData = JSON.parse(user);
     console.log("check decodedData: ", decodedData);
 
-    const paymentUrl = `https://napas.com.vn/pay?orderId=${orderInfo.orderId}&amount=500000`;
     useEffect(() => {
         try {
             const decoded = JSON.parse(Base64.decode(encodedData));
@@ -77,8 +72,8 @@ const Payment = () => {
         <div className="payment-container">
             <div className="qr-code">
                 <h2>Quét mã để thanh toán</h2>
-                <QRCodeCanvas value={paymentUrl} size={280} />
-                <p>Vui lòng quét mã QR trên ứng dụng Napas để hoàn tất thanh toán.</p>
+                <img src={QRcode} />
+                <p>Chuyển khoản với nội dung được viết sẵn bên cạnh để hoàn tất thanh toán.</p>
             </div>
 
             <div className="order-info">
@@ -91,6 +86,7 @@ const Payment = () => {
                 <p><strong>Ghế:</strong> {decodedData?.seatNumbers?.join(", ")}</p>
                 <p><strong>Tên khách hàng:</strong> {userData?.Name}</p>
                 <p><strong>Tổng tiền:</strong> {decodedData?.seatNumbers.length * showtimeSelected?.Price}</p>
+                <p><strong>Nội dung chuyển tiền:</strong> {userData?.UserId}&{showtimeSelected?.ShowtimeId}&{decodedData?.seatNumbers?.join("&")}&{room?.RoomId}&{theater?.TheaterId}</p>
                 <button className='payment-button' onClick={() => { handleClickPaid() }}>Đã thanh toán</button>
             </div>
 
